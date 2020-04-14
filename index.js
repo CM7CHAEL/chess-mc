0ee377084e6e029a1569251b9a612499961b2c15
Michael Cervera <michaelcervera13@gmail.com>
2020-04-14 12:40:33 -0500

    details

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  //https://imgbb.com/
+  el: '#app',
+  data: {
+    books: [{
+        img: 'https://www.ajedrezeureka.com/wp-content/uploads/2016/02/ajedrez-posicion-de-todas-las-piezas-600x326.jpg',
+        title: 'Inicialización del ajedrez',
+        fecha: 'Europa durante el siglo XV',
+        description: 'El ajedrez es un juego entre dos contrincantes, cada uno dispone de 16 piezas móviles que se colocan sobre un tablero1​ dividido en 64 casillas o escaques',
+        link: 'https://drive.google.com/file/d/10zp9_Us_5wwcZe7_NEcQkVuMQuLCsUW_/view?usp=sharing'
+      }
+    ]
+  }
+})
